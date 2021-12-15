1019e6000f631187b0aaf07462f477b481852fea
SJRXD <rohr.olasz@gmail.com>
2021-12-15 09:58:39 -0800

    basic react index.js setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './src/index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
